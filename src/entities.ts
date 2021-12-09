655c2aa78aa2b0ee9c2272ea132206d4b4d4ef24
Jeroen Wouters <jeroenwouters@inuits.eu>
2021-12-09 12:21:42 +0100

    add duplicate filenamecheck

--- src/entities.ts
+++ src/entities.ts
@@ -39,7 +39,20 @@ export class EntitiesAPI extends RESTDataSourceWithStaticToken<Context> {
 
   async getMediafiles(id: string): Promise<MediaFile[]> {
     if (id !== 'noid') {
-      return await this.get(`entities/${id}/mediafiles`);
+      const mediafiles = await this.get(`entities/${id}/mediafiles`);
+      // a Set to track seen mediafiles
+      const seen = new Set();
+
+      const filtered = mediafiles.filter((mediafile: MediaFile) => {
+        // check if the current mediafile is a duplicate
+        const isDuplicate: boolean = seen.has(mediafile.filename);
+        // add the current brand to the Set
+        seen.add(mediafile.filename);
+        // filter() returns the brand when isDuplicate is false
+        return !isDuplicate;
+      });
+
+      return filtered;
     } else {
       return [];
     }
