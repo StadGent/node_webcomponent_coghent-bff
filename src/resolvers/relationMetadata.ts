8e26ebaa394ff1c7041cea5bc84b09f70b5597f4
Berend <berend@inuits.eu>
2022-08-08 09:38:15 +0200

    Refs #96254 change primary_transcode_location to transcode_file_location

--- src/resolvers/relationMetadata.ts
+++ src/resolvers/relationMetadata.ts
@@ -1,56 +1,91 @@
 import { setEntitiesIdPrefix } from '../common';
 import { relationsWithExcludedCollections } from '../parsers/entities';
-import { Collections, Entity, MediaFile, Relation, RelationType } from '../type-defs';
+import {
+  Collections,
+  Entity,
+  MediaFile,
+  Relation,
+  RelationType,
+} from '../type-defs';
 import { DataSources } from '../types';
 
-export const setMediafileOnAsset = async (_dataSources: DataSources, _assets: Array<Entity>, _frameId: string) => {
-  const updatedAssets: Array<Entity> = _assets
-  let components = await _dataSources.EntitiesAPI.getRelationOfType(_frameId, RelationType.Components);
-  components = relationsWithExcludedCollections(components, [Collections.Mediafiles])
-  const itemsToUpdate: Array<{ id: string, index: number, mediaFile: MediaFile, setMediafile: number }> = []
+export const setMediafileOnAsset = async (
+  _dataSources: DataSources,
+  _assets: Array<Entity>,
+  _frameId: string
+) => {
+  const updatedAssets: Array<Entity> = _assets;
+  let components = await _dataSources.EntitiesAPI.getRelationOfType(
+    _frameId,
+    RelationType.Components
+  );
+  components = relationsWithExcludedCollections(components, [
+    Collections.Mediafiles,
+  ]);
+  const itemsToUpdate: Array<{
+    id: string;
+    index: number;
+    mediaFile: MediaFile;
+    setMediafile: number;
+  }> = [];
   for (const [index, _component] of components.entries()) {
-    if (_component.setMediafile && _component.setMediafile != 1 && !_component.key.includes('mediafiles')) {
+    if (
+      _component.setMediafile &&
+      _component.setMediafile != 1 &&
+      !_component.key.includes('mediafiles')
+    ) {
       const mediafiles = await _dataSources.EntitiesAPI.getMediafiles(
         setEntitiesIdPrefix(_component.key)
-      )
+      );
       if (mediafiles[_component.setMediafile - 1]) {
-        const mediafile = mediafiles[_component.setMediafile - 1]
+        const mediafile = mediafiles[_component.setMediafile - 1];
         itemsToUpdate.push({
           id: setEntitiesIdPrefix(_component.key, false),
           index: index,
           mediaFile: mediafile,
-          setMediafile: _component.setMediafile
-        })
+          setMediafile: _component.setMediafile,
+        });
       }
     }
   }
   if (itemsToUpdate.length >= 1) {
     for (const item of itemsToUpdate) {
-      if (updatedAssets[item.index] && updatedAssets[item.index].id === item.id) {
-        updatedAssets[item.index].primary_mediafile_location = item.mediaFile.original_file_location
-        updatedAssets[item.index].primary_mediafile = item.mediaFile.filename
-        updatedAssets[item.index].primary_height = item.mediaFile.img_height
-        updatedAssets[item.index].primary_width = item.mediaFile.img_width
-        updatedAssets[item.index].primary_transcode = item.mediaFile.transcode_filename
-        updatedAssets[item.index].primary_transcode_location = item.mediaFile.primary_transcode_location
+      if (
+        updatedAssets[item.index] &&
+        updatedAssets[item.index].id === item.id
+      ) {
+        updatedAssets[item.index].primary_mediafile_location =
+          item.mediaFile.original_file_location;
+        updatedAssets[item.index].primary_mediafile = item.mediaFile.filename;
+        updatedAssets[item.index].primary_height = item.mediaFile.img_height;
+        updatedAssets[item.index].primary_width = item.mediaFile.img_width;
+        updatedAssets[item.index].primary_transcode =
+          item.mediaFile.transcode_filename;
+        updatedAssets[item.index].primary_transcode_location =
+          item.mediaFile.transcode_file_location;
       }
     }
-
   }
-  return updatedAssets
-}
+  return updatedAssets;
+};
 
-export const createRelationOfId = (_entityId: string, _relationType: RelationType) => {
+export const createRelationOfId = (
+  _entityId: string,
+  _relationType: RelationType
+) => {
   const relation = {
     key: `entities/${_entityId}`,
     type: _relationType,
-  } as Relation
-  return relation
-}
+  } as Relation;
+  return relation;
+};
 
-export const addTimingsToRelation = (_relation: Relation, _timings: Record<'start' | 'zoom' | 'end', number>) => {
-  _relation.timestamp_start = _timings['start']
-  _relation.timestamp_zoom = _timings['zoom']
-  _relation.timestamp_end = _timings['end']
-  return _relation
-}
+export const addTimingsToRelation = (
+  _relation: Relation,
+  _timings: Record<'start' | 'zoom' | 'end', number>
+) => {
+  _relation.timestamp_start = _timings['start'];
+  _relation.timestamp_zoom = _timings['zoom'];
+  _relation.timestamp_end = _timings['end'];
+  return _relation;
+};
--- src/resolvers/upload.ts
+++ src/resolvers/upload.ts
@@ -1,57 +1,77 @@
-import { getMetadataOfKey } from '../parsers/entities'
-import { PublicationStatus } from '../sources/constants'
-import { Entity, KeyValuePair, MediaFile, Metadata, MetaKey, Publication, Relation } from '../type-defs'
+import { getMetadataOfKey } from '../parsers/entities';
+import { PublicationStatus } from '../sources/constants';
+import {
+  Entity,
+  KeyValuePair,
+  MediaFile,
+  Metadata,
+  MetaKey,
+  Publication,
+  Relation,
+} from '../type-defs';
 
-const USER_MEDIAFILE_NAME_PREFIX = 'user-uploaded-'
-const NO_IMAGE_PATH = './no-image.png'
+const USER_MEDIAFILE_NAME_PREFIX = 'user-uploaded-';
+const NO_IMAGE_PATH = './no-image.png';
 
 export const getPublicationKeyFromValue = (publication_status: string) => {
   return new Promise((resolve, reject) => {
     for (const key of Object.values(Publication)) {
       if (publication_status === PublicationStatus[key]) {
-        resolve(key)
+        resolve(key);
       }
     }
-    resolve(Publication.Private)
-  })
-}
+    resolve(Publication.Private);
+  });
+};
 
 export const getMediafileLink = (_mediafiles: Array<MediaFile>) => {
-  let mediafileLink = NO_IMAGE_PATH
+  let mediafileLink = NO_IMAGE_PATH;
   if (_mediafiles.length >= 1) {
-    if (_mediafiles[0].primary_transcode_location) {
-      mediafileLink = _mediafiles[0].primary_transcode_location
+    if (_mediafiles[0].transcode_file_location) {
+      mediafileLink = _mediafiles[0].transcode_file_location;
     } else if (_mediafiles[0].original_file_location)
-      mediafileLink = _mediafiles[0].original_file_location
+      mediafileLink = _mediafiles[0].original_file_location;
   }
-  mediafileLink = encodeURI(mediafileLink)
-  return mediafileLink
-}
+  mediafileLink = encodeURI(mediafileLink);
+  return mediafileLink;
+};
 
-export const getRightFromMediafile = (_mediafiles: Array<MediaFile>, _fileLocation: string | null) => {
-  let rights = null
+export const getRightFromMediafile = (
+  _mediafiles: Array<MediaFile>,
+  _fileLocation: string | null
+) => {
+  let rights = null;
   if (_mediafiles.length >= 1 && _fileLocation !== null) {
-    const mediafile = _mediafiles.find(mediafile => mediafile.primary_transcode_location === _fileLocation || mediafile.original_file_location === _fileLocation)
-    mediafile !== undefined ? rights = getMetadataOfKey(mediafile as unknown as Entity, MetaKey.Rights) : null
+    const mediafile = _mediafiles.find(
+      (mediafile) =>
+        mediafile.transcode_file_location === _fileLocation ||
+        mediafile.original_file_location === _fileLocation
+    );
+    mediafile !== undefined
+      ? (rights = getMetadataOfKey(
+          mediafile as unknown as Entity,
+          MetaKey.Rights
+        ))
+      : null;
   }
-  return rights
-}
+  return rights;
+};
 
 export const removePrefixFromMetadata = (_metadata: Array<Metadata>) => {
-  const updatedMetadata = []
+  const updatedMetadata = [];
   for (const metadata of _metadata) {
-    let stripped = metadata.value
+    let stripped = metadata.value;
 
     if (metadata.value !== null) {
       if (stripped!.includes(USER_MEDIAFILE_NAME_PREFIX)) {
-        stripped = stripped!.replace(USER_MEDIAFILE_NAME_PREFIX, "")
+        stripped = stripped!.replace(USER_MEDIAFILE_NAME_PREFIX, '');
       }
     }
     updatedMetadata.push({
       key: metadata.key,
       value: stripped,
       label: metadata.label,
-    } as Metadata)
+    } as Metadata);
   }
-  return updatedMetadata
-}
+  return updatedMetadata;
+};
