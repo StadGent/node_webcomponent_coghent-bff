353116a2674940b162b29366c77bbbeb1c71c27f
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-08-16 12:25:34 +0200

    remove getentities context log + bump apollo auth to 1.0.7

--- src/entities_search.ts
+++ src/entities_search.ts
@@ -16,7 +16,6 @@ export class SearchAPI extends AuthRESTDataSource<Context> {
     searchValue: SearchFilter,
     fetchPolicy: string
   ): Promise<EntitiesResults> {
-    console.log(`\n CONTEXT GET ENTITIES`, this.context);
     let body = searchValue;
     const data = await this.post(
       `${this.SEARCH}/collection?limit=${limit}&skip=${skip}`,
