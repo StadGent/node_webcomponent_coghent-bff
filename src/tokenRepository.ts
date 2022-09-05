f705897e5943ed228bbd2dcf8c9f454e4c9a2bd9
Berend <berend@inuits.eu>
2022-09-05 10:22:37 +0200

    Fix build

--- src/tokenRepository.ts
+++ src/tokenRepository.ts
@@ -5,62 +5,62 @@ import jwt_decode from 'jwt-decode';
 import { environment } from './environment';
 
 export interface TokenResponse {
-  access_token: string,
-  refresh_token: string,
-  id_token: string,
-  tokenEndpoint: string,
-  redirectUri: string
+  access_token: string;
+  refresh_token: string;
+  id_token: string;
+  tokenEndpoint: string;
+  redirectUri: string;
 }
 
 export interface AuthBody {
-  code: string,
-  grant_type: string,
-  client_id: string,
-  client_secret: string,
-  redirect_uri: string
+  code: string;
+  grant_type: string;
+  client_id: string;
+  client_secret: string;
+  redirect_uri: string;
 }
 
 export interface RefreshBody {
-  grant_type: string,
-  client_id: string,
-  refresh_token: string,
-  oidc_url: string
+  grant_type: string;
+  client_id: string;
+  refresh_token: string;
+  oidc_url: string;
 }
 
 export interface TokenUser {
-  name: string,
-  preferred_username: string,
-  given_name: string,
-  family_name: string,
-  email: string
+  name: string;
+  preferred_username: string;
+  given_name: string;
+  family_name: string;
+  email: string;
 }
 
 export class TokenManager {
   accessToken?: string;
   refreshToken?: string;
 
-  constructor(
-    private clientId: string,
-    private tokenEndpoint: string,
-  ) { }
+  constructor(private clientId: string, private tokenEndpoint: string) {}
 
   async authenticate(authCode: string, redirectUri: string) {
     console.log(environment.oauthBaseUrl, this.tokenEndpoint);
     console.log(this.clientId, environment.clientSecret);
-    const res = await fetch(`${environment.oauthBaseUrl}${this.tokenEndpoint}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: new URLSearchParams({
-        grant_type: 'authorization_code',
-        code: authCode,
-        client_id: this.clientId,
-        client_secret: environment.clientSecret,
-        redirect_uri: redirectUri,
-      }),
-    });
-    const data = await res.json() as TokenResponse;
+    const res = await fetch(
+      `${environment.oauthBaseUrl}${this.tokenEndpoint}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+          grant_type: 'authorization_code',
+          code: authCode,
+          client_id: this.clientId,
+          client_secret: environment.clientSecret,
+          redirect_uri: redirectUri,
+        }),
+      }
+    );
+    const data = (await res.json()) as TokenResponse;
     if (!data.access_token || !data.refresh_token) {
       throw new Error('Invalid response from OpenID server');
     }
@@ -71,19 +71,21 @@ export class TokenManager {
   }
 
   async refresh(): Promise<TokenResponse> {
-    const res = await fetch(`${environment.oauthBaseUrl}${this.tokenEndpoint}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: new URLSearchParams({
-        grant_type: 'refresh_token',
-        refresh_token: this.refreshToken,
-        client_id: this.clientId,
-        oidc_url: this.tokenEndpoint,
-      }),
-    });
-    return await res.json() as TokenResponse;
+    const res = await fetch(
+      `${environment.oauthBaseUrl}${this.tokenEndpoint}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+          grant_type: 'refresh_token',
+          refresh_token: this.refreshToken ?? '',
+          client_id: this.clientId,
+          oidc_url: this.tokenEndpoint,
+        }),
+      }
+    );
+    return (await res.json()) as TokenResponse;
   }
 }
-
