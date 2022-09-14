c4e88e0c55d355d75d59079ea883984e754b0e82
Jeroen Wouters <jeroenwouters@inuits.eu>
2022-09-14 18:52:24 +0000

    replace Thesaurus with Getty

--- src/resolvers/search.ts
+++ src/resolvers/search.ts
@@ -4,22 +4,19 @@ export type ParsedFilter = {
   key?: MetaKey;
   type: AdvancedInputType;
   value: Array<EntityTypes>;
-  item_types: string[]
-}
+  item_types: string[];
+};
 
 export const getRelationsForUpload = (_searchValue: string) => {
-  const itemTypes = [
-    EntityTypes.Thesaurus,
-    EntityTypes.Person,
-  ]
+  const itemTypes = [EntityTypes.Getty, EntityTypes.Person];
 
-  const filters: Array<ParsedFilter> = []
+  const filters: Array<ParsedFilter> = [];
   const titleFilter = {
     type: AdvancedInputType.TextInput,
     value: _searchValue,
     item_types: itemTypes,
-  }
+  };
 
-  filters.push(titleFilter as unknown as ParsedFilter)
-  return filters
-}
+  filters.push(titleFilter as unknown as ParsedFilter);
+  return filters;
+};
