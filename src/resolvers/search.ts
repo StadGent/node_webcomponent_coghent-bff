143f9524dabb9773a35b06af54ee3b09f0760ff9
Jeroen Wouters <jeroenwouters@inuits.eu>
2022-09-14 19:01:08 +0000

    change format for advanced filter search upload relations

--- src/resolvers/search.ts
+++ src/resolvers/search.ts
@@ -11,11 +11,17 @@ export const getRelationsForUpload = (_searchValue: string) => {
   const itemTypes = [EntityTypes.Getty, EntityTypes.Person];
 
   const filters: Array<ParsedFilter> = [];
-  const titleFilter = {
-    type: AdvancedInputType.TextInput,
-    value: _searchValue,
-    item_types: itemTypes,
-  };
+  const titleFilter = [
+    {
+      type: AdvancedInputType.TextInput,
+      item_types: itemTypes,
+    },
+    {
+      type: AdvancedInputType.TextInput,
+      value: _searchValue,
+      key: 'title',
+    },
+  ];
 
   filters.push(titleFilter as unknown as ParsedFilter);
   return filters;
