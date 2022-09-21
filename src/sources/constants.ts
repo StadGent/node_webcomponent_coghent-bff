1ebbfa708b9e5c54c24cd1c64ff9566d6bf01f36
Berend <berend@inuits.eu>
2022-09-21 12:08:16 +0200

    Refs #98918 Add declined publication status

--- src/sources/constants.ts
+++ src/sources/constants.ts
@@ -1,15 +1,16 @@
-import { Rights, Publication } from '../type-defs'
+import { Rights, Publication } from '../type-defs';
 
 export const License: Record<Rights, string> = {
-  [Rights.Cc0]: "CC0 1.0",
-  [Rights.Undetermined]: "To be determined",
-}
+  [Rights.Cc0]: 'CC0 1.0',
+  [Rights.Undetermined]: 'To be determined',
+};
 
 export const PublicationStatus: Record<Publication, string> = {
-  [Publication.Public]: "publiek",
-  [Publication.Private]: "niet-publiek",
-  [Publication.Validate]: "te valideren",
-}
+  [Publication.Public]: 'publiek',
+  [Publication.Private]: 'niet-publiek',
+  [Publication.Validate]: 'te valideren',
+  [Publication.Declined]: 'afgewezen',
+};
 
-export const SKIP_RELATIONS = 'skip_relations'
-export const SIXTH_COLLECTION = 'sixth_collection'
+export const SKIP_RELATIONS = 'skip_relations';
+export const SIXTH_COLLECTION = 'sixth_collection';
