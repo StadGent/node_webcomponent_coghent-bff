2ab643763894f8aa0588d74b2e7687df06db3c08
Berend <berend@inuits.eu>
2022-10-27 09:59:23 +0200

    Refs #101294 Set higher limit for express

--- src/main.ts
+++ src/main.ts
@@ -45,6 +45,7 @@ if (environment.redisHost) {
 
 export const app = express();
 app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ limit: '25mb', extended: true }));
 app.use(
   cors({
     credentials: false,
